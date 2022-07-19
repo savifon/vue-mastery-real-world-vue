6ed8e7c7c4b0b3c6a17a179ce1cad76cf05b4ee0
savio.fonseca <savio.fonseca@ucl.br>
2022-07-19 11:56:51 -0300

    fix: lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': [
-      'none',
+      'off',
       {
         ignores: [],
       },
